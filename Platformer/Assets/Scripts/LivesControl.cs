98097bf04ced22d897934b1f8893ee92fc85549d
aaronm-12 <aarondmc12@gmail.com>
2019-03-07 14:52:18 -0700

    Persistent Lives

---
+++ Platformer/Assets/Scripts/LivesControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesControl : MonoBehaviour
+{
+    private Text lives;
+    private string livesTag = "Lives: ";
+
+    private void Awake()
+    {
+        lives = GetComponent<Text>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives.text = livesTag + MasterControl.Instance.lives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lives.text = livesTag + MasterControl.Instance.lives;
+    }
+}
---
+++ Platformer/Assets/Scripts/MasterControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterControl : MonoBehaviour
+{
+    public static MasterControl Instance;
+
+    public int lives = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
