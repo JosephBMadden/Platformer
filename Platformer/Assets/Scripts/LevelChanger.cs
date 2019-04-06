4094e067b642d2fed3fea226262d8e371d02c68c
aaronm-12 <aarondmc12@gmail.com>
2019-04-06 11:18:11 -0600

    Start of dynamic levels

---
+++ Platformer/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+    public GameObject foreOrange;
+    public GameObject foreBlack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreOrange.SetActive(true);
+        foreBlack.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(foreOrange.active)
+            {
+                foreOrange.SetActive(false);
+                Debug.Log("disabled black");
+            }
+        }
+    }
+}
